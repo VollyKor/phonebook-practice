b1b8c696ed0b477347148735c125726d7f5d5c37
Volodymyr Kornieiev <Volydemar2032@gmail.com>
2021-01-11 16:51:32 +0200

    добавлен бутстрап

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,27 @@
 import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 export default function NavBar() {
   return (
     <header>
-      <nav>
-        <ul style={{ display: 'flex' }}>
-          <li className="nav-list-item">
+      <Navbar bg="light" expand="lg">
+        <Nav className="mr-auto">
+          <NavItem className="nav-list-item">
             <NavLink to="/phonebook">Телефонная книга</NavLink>
-          </li>
-          <li className="nav-list-item">
+          </NavItem>
+          <NavItem className="nav-list-item">
             <NavLink to="/notes">Заметки</NavLink>
-          </li>
-          <li className="nav-list-item">
+          </NavItem>
+          <NavItem className="nav-list-item">
             <NavLink to="/">Что-то еще</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <hr />
+          </NavItem>
+        </Nav>
+      </Navbar>
     </header>
   );
 }
+
+<Nav className="mr-auto">
+  <Nav.Link href="#home">Home</Nav.Link>
+  <Nav.Link href="#link">Link</Nav.Link>
+</Nav>;
