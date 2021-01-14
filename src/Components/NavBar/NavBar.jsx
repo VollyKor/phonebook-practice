faf4f1545bf39946a981197f99055d98cd951c01
Volodymyr Kornieiev <Volydemar2032@gmail.com>
2021-01-14 12:12:16 +0200

    Revert "добавлен бутстрап"
    This reverts commit b1b8c696ed0b477347148735c125726d7f5d5c37.

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,22 @@
 import { NavLink } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 export default function NavBar() {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
-        <Nav className="mr-auto">
-          <NavItem className="nav-list-item">
+      <nav>
+        <ul style={{ display: 'flex' }}>
+          <li className="nav-list-item">
             <NavLink to="/phonebook">Телефонная книга</NavLink>
-          </NavItem>
-          <NavItem className="nav-list-item">
+          </li>
+          <li className="nav-list-item">
             <NavLink to="/notes">Заметки</NavLink>
-          </NavItem>
-          <NavItem className="nav-list-item">
+          </li>
+          <li className="nav-list-item">
             <NavLink to="/">Что-то еще</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+          </li>
+        </ul>
+      </nav>
+      <hr />
     </header>
   );
 }
-
-<Nav className="mr-auto">
-  <Nav.Link href="#home">Home</Nav.Link>
-  <Nav.Link href="#link">Link</Nav.Link>
-</Nav>;
