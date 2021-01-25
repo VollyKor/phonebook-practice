6a11db6093029e4119858e115d958a4f6af7ce5e
Warness <65484013+Warness@users.noreply.github.com>
2021-01-25 21:27:58 +0200

    Правки datapicker а

--- src/Components/Todos/AddTodoForm/AddTodoForm.jsx
+++ src/Components/Todos/AddTodoForm/AddTodoForm.jsx
@@ -1,7 +1,13 @@
 import Button from 'Components/Button/Button';
+import 'react-datepicker/dist/react-datepicker.css';
 import s from './AddTodoForm.module.scss';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 
 export default function AddTodoForm() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
+
   return (
     <form className={s.form}>
       <h2 className={s.title}>Add Todo to your list</h2>
@@ -13,6 +19,20 @@ export default function AddTodoForm() {
         <span className={s.subtitle}>Описание</span>
         <textarea className={s.textarea} />
       </label>
+      <label className={s.label}>
+        <span className={s.subtitle}>Дедлайн</span>
+        <DatePicker
+          selected={startDate}
+          onChange={date => setStartDate(date)}
+          timeInputLabel="Time:"
+          dateFormat="MM/dd/yyyy h:mm aa"
+          timeFormat="p"
+          showTimeInput
+          className={s.input}
+          closeOnScroll={true}
+        />
+      </label>
+
       <Button type="submit" className={s.btn}>
         Add Todo
       </Button>
